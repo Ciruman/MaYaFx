bd938295f546f7bbd9c22126ec05e906dea534a0
Ciruman <dicir00@gmail.com>
2016-02-16 22:53:01 +0100

    basic model created

--- src/main/java/com/ciruman/model/MindMapNode.java
+++ src/main/java/com/ciruman/model/MindMapNode.java
@@ -1,7 +1,21 @@
 package com.ciruman.model;
 
-/**
- * Created by yago on 16/02/16.
- */
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class MindMapNode {
+
+    ObservableList<MindMapNode> childs = FXCollections.observableArrayList();
+
+    public ObservableList<MindMapNode> getChilds() {
+        return childs;
+    }
+
+    public void setChilds(ObservableList<MindMapNode> childs) {
+        this.childs = childs;
+    }
+
+    public ObservableList<MindMapNode> childsProperty(){
+        return childs;
+    }
 }
